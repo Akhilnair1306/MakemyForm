f936820acbbd50957e340715ee7f1360d7e92631
Akhilnair1306 <111660827+Akhilnair1306@users.noreply.github.com>
2024-06-27 17:27:44 +0530

    more

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import CreateForm from './_components/CreateForm';
 import FormList from './_components/FormList';
 
+
+
 function Dashboard() {
+
+  const [openDialog, setOpenDialog] = useState(false);
   return (
     <div className='p-10'>
       <h2 className='font-bold text-3xl flex items-center justify-between'>Dashboard
-        <CreateForm />
+      <Button onClick={() => setOpenDialog(true)}>+ Create Form</Button>
       </h2>
       <FormList />
+      <CreateForm open={openDialog} onClose={() => setOpenDialog(false)} />
     </div>
   )
 }
