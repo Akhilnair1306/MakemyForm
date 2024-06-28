816a1d0d9fc677cb52d1d91fa4abd21aea0bcd8d
Akhilnair1306 <111660827+Akhilnair1306@users.noreply.github.com>
2024-06-28 15:54:53 +0530

    few more fixes

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,11 +1,11 @@
 "use client"
 import React, { useState } from 'react';
-  // Adjust the import path as needed
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
 import CreateForm from '../dashboard/_components/CreateForm';
+import { SignInButton, useUser } from '@clerk/nextjs';
 
 function Hero() {
+    const { user, isSignedIn } = useUser();
     const [openDialog, setOpenDialog] = useState(false);
 
     return (
@@ -21,17 +21,23 @@ function Hero() {
                         <p className="mt-4 sm:text-xl/relaxed">
                             Generate dynamic forms with unparalleled accuracy using AI technology. Simplify form creation, ensure precision, and effortlessly capture data with intelligently designed forms tailored to your needs.
                         </p>
-
-                        <div className="mt-8 flex flex-wrap justify-center gap-4">
-                            <Button onClick={() => setOpenDialog(true)}>+ Create Form</Button>
-
-                            <a
-                                className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-                                href="#"
-                            >
-                                Learn More
-                            </a>
-                        </div>
+                        {isSignedIn ? (
+                            <div className="mt-8 flex flex-wrap justify-center gap-4">
+                                <Button onClick={() => setOpenDialog(true)}>+ Create Form</Button>
+                                <a
+                                    className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                                    href="#"
+                                >
+                                    Learn More
+                                </a>
+                            </div>
+                        ) : (
+                            <div className='gap-4 mt-8'>
+                            <SignInButton>
+                                <Button>Get Started</Button>
+                            </SignInButton>
+                            </div>
+                        )}
                     </div>
                 </div>
             </section>
@@ -41,3 +47,4 @@ function Hero() {
 }
 
 export default Hero;
+
